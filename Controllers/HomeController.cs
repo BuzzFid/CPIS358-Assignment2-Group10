c50bb5b0343c36792528f8ec83acaafa5790baaf
BuzzFid <abdalmalek2005@gmail.com>
2025-12-04 06:19:25 +0300

    I just added the Cookies

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,6 +8,18 @@ namespace FCIT_Event_Finder.Controllers
     {
         public IActionResult Index()
         {
+            // Check if the user is authenticated
+            if (User.Identity.IsAuthenticated)
+            {
+                // User is logged in, get their username
+                SetCookies("userName", User.Identity.Name);
+            }
+            else
+            {
+                // User is not logged in, set a cookie with "Guest"
+                SetCookies("userName", "guest");
+            }
+            SetCookies("broswerName", Request.Headers["User-Agent"].ToString());
             return View();
         }
 
@@ -26,5 +38,22 @@ namespace FCIT_Event_Finder.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult SetCookies(string cookieName, string cookieValue)
+        {
+            CookieOptions options = new CookieOptions
+            {
+                Expires = DateTime.Now.AddDays(15), // Cookie expires in 14 days
+                HttpOnly = true,
+          
+            Secure = true,
+                SameSite = SameSiteMode.Strict
+               
+            };
+            Response.Cookies.Append(cookieName, cookieValue, options);
+            return Ok("Cookies has been set.");
+        }
+
+
     }
 }
