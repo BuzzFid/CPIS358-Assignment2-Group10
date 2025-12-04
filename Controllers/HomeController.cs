7e6faecadc02a1c357469a104f0ef471cb244ff7
BuzzFid <abdalmalek2005@gmail.com>
2025-12-04 14:42:50 +0300

    Add session and cookie management to controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
-using System.Diagnostics;
 using FCIT_Event_Finder.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Tasks;
+using System.Diagnostics;
 
 namespace FCIT_Event_Finder.Controllers
 {
@@ -8,19 +9,26 @@ namespace FCIT_Event_Finder.Controllers
     {
         public IActionResult Index()
         {
+            SetSession("id", Guid.NewGuid().ToString());
             // Check if the user is authenticated
             if (User.Identity.IsAuthenticated)
             {
                 // User is logged in, get their username
                 SetCookies("userName", User.Identity.Name);
+                // create a session with username
+                SetSession("username", User.Identity.Name);
+
             }
             else
             {
                 // User is not logged in, set a cookie with "Guest"
                 SetCookies("userName", "guest");
+                // create a session with username
+                SetSession("username", "guest");
             }
+            // Get the broswer type
             SetCookies("broswerName", Request.Headers["User-Agent"].ToString());
-            return View();
+              return View();
         }
 
         public IActionResult Contact()
@@ -54,6 +62,11 @@ namespace FCIT_Event_Finder.Controllers
             return Ok("Cookies has been set.");
         }
 
-
+        public IActionResult SetSession(string key, string value)
+        {
+            // Set session value
+            HttpContext.Session.SetString(key, value);
+            return RedirectToAction("Index");
+        }
     }
 }
